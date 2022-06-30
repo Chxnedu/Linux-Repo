6427ae704d64283f46f29c026d3d2b59127441be
Chxnedu <ojichinedu4@gmail.com>
2022-06-30 10:10:17 -0400

    added a kops directory

---
+++ Kubernetes/Kops/kops_setup.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+# script to setup your kops server
+
+sudo apt update && apt install awscli
+
+aws --version
+
+aws configure
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+kubectl version --client
+
+curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+
+chmod +x kops-linux-amd64
+
+sudo mv kops-linux-amd64 /usr/local/bin/kops
+
+kops --help
+
+
+
+
