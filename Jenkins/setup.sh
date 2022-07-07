5f9e6d7ca2b7edf7ce25d6e63eb4ab592229e578
Chxnedu <ojichinedu4@gmail.com>
2022-07-07 12:50:06 -0400

    added jenkins directory

---
+++ Jenkins/setup.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+# script to setup your jenkins server on a vm
+
+sudo apt update -y && sudo apt-get update
+
+sudo apt install openjdk-11-jdk
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add
+
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' 
+
+sudo apt update
+
+sudo apt install jenkins
+
+sudo systemctl start jenkins
+
+sudo systemctl status jenkins
+
+sleep 10
+
+sudo ufw allow 8080
+
+sudo ufw status
+
+exit
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
