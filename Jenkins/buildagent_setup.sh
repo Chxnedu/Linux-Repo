bb45721a4af8d3f2b0801975ba157226a1b35801
Chxnedu <ojichinedu4@gmail.com>
2022-07-09 15:54:38 -0400

    made some changes

--- Jenkins/buildagent_setup.sh
+++ Jenkins/buildagent_setup.sh
@@ -5,3 +5,16 @@ sudo apt update -y && sudo apt-get update
 
 sudo apt install openjdk-11-jdk
 
+echo "What do you want to call the new user?"
+read username
+
+sudo useradd -m $username
+
+sudo -u $username mkdir /home/$username/.ssh
+
+echo "Paste public ssh key"
+sleep 2
+
+sudo -u $username nano /home/$username/.ssh/authorized_keys
+
+
