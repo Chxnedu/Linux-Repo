7a8638e7cafa66bdabfebff6a14f950a75ccb250
Chxnedu <ojichinedu4@gmail.com>
2024-07-30 15:17:45 +0100

    Update bestdayever.sh

--- BashScripts/bestdayever.sh
+++ BashScripts/bestdayever.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # a script challenge
+# this is an example of an argument. the $1 and $2 are arguments that take inputs from what you specify when you run the script
 name=$1
 compliment=$2
 user=$(whoami)
