a3b4692539d953a764ac86a8595d9ca5563459be
Chxnedu <ojichinedu4@gmail.com>
2024-07-30 11:37:42 +0100

    Create fuctions.sh

---
+++ BashScripts/fuctions.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# we are going to create functions in this script
+# to create a function
+enter_directory() {
+    pwd
+    cd /random/path
+}
+
+ssh blah blah
+enter_directory
+# to call a function, just use the name you gave it
